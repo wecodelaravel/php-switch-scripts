e39281688df04fbce3c8a0d37bf7292ec068a610
Phillip Madsen <apcoder2019@gmail.com>
2022-06-20 19:02:51 -0600

    updated to use 8.1

--- setup.sh
+++ setup.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+echo "install Git for ubuntu"
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update > /dev/null
+sudo apt-get install git -y  > /dev/null
+echo "install Git LFS for ubuntu"
+sudo apt-get install git-lfs -y  > /dev/null
+
+
+# INSTALL SUBLIMETEXT INTO DOCKER AND UBUNTU
+echo "install Sublime for ubuntu"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update  > /dev/null
+sudo apt-get install sublime-text -y > /dev/null
+
+echo "export EDITOR='subl -w'" >> /home/code/.bashrc
+source /home/code/.bashrc
+
+# touch ~/.config/sublime-text-3/Packages/User/Package Control.sublime-settings
+# ADD PROJECT DIRECTORY TO SUBL
+subl -a /home/code/codecorp_docker
+
+
 echo "* Refreshing software repositories..."
 sudo apt-get update > /dev/null
 
@@ -58,7 +82,17 @@ sudo apt-get install -y php8.0-bz2 php8.0-curl php8.0-gd php8.0-mbstring php8.0-
 echo "* Installing additional PHP extensions..."
 sudo apt-get install -y php-memcache php-memcached php-redis  > /dev/null
 
+echo "* Installing PHP 8.1..."
+sudo apt-get install -y php8.1 php8.1-common php8.1-cli > /dev/null
+
+echo "* Installing PHP 8.1 extensions..."
+sudo  apt-get install -y --no-install-recommends libmagickwand-dev
+sudo apt-get install -y php8.1-bz2 php8.1-curl php8.1-gd php8.1-mbstring php8.1-mysql php8.1-opcache php8.1-readline php8.1-soap php8.1-sqlite3 php8.1-tidy php8.1-xml php8.1-xsl php8.1-zip > /dev/null
+
+echo "* Installing additional PHP extensions..."
+sudo apt-get install -y php-memcache php-memcached php-redis  > /dev/null
+
 echo "* Setup complete. You may now use the 'switch-to-php-*.*.sh' scripts."
 
-# echo "installing Rust"
+# echo "Installing Rust"
 # curl https://sh.rustup.rs -sSf | sh -s -- -y
---
+++ switch-to-php-8.1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "* Disabling Apache PHP 5.6 module..."
+sudo a2dismod php5.6 > /dev/null
+
+echo "* Disabling Apache PHP 7.0 module..."
+sudo a2dismod php7.0 > /dev/null
+
+echo "* Disabling Apache PHP 7.1 module..."
+sudo a2dismod php7.1 > /dev/null
+
+echo "* Disabling Apache PHP 7.2 module..."
+sudo a2dismod php7.2 > /dev/null
+
+echo "* Disabling Apache PHP 7.3 module..."
+sudo a2dismod php7.3 > /dev/null
+
+echo "* Disabling Apache PHP 7.4 module..."
+sudo a2dismod php7.4 > /dev/null
+
+echo "* Disabling Apache PHP php8.0 module..."
+sudo a2dismod php8.0 > /dev/null
+
+echo "* Enabling Apache PHP 8.0 module..."
+sudo a2enmod php8.1 > /dev/null
+
+echo "* Restarting Apache..."
+sudo service apache2 restart > /dev/null
+
+echo "* Switching CLI PHP to 8.1..."
+sudo update-alternatives --set php /usr/bin/php8.1 > /dev/null
+
+echo "* Switch to PHP 8.1 complete."
