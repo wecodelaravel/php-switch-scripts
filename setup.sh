19310a2d1d948b77df69b15e6cb725bb91da6ef7
Phillip Madsen <58571584+ap-coder@users.noreply.github.com>
2022-11-22 11:27:28 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 sail #!/bin/bash
 
-echo "install Git for ubuntu"
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update > /dev/null
-sudo apt-get install git -y  > /dev/null
-echo "install Git LFS for ubuntu"
-sudo apt-get install git-lfs -y  > /dev/null
+# echo "install Git for ubuntu"
+# sudo add-apt-repository ppa:git-core/ppa
+# sudo apt-get update > /dev/null
+# sudo apt-get install git -y  > /dev/null
+# echo "install Git LFS for ubuntu"
+# sudo apt-get install git-lfs -y  > /dev/null
 
 
 # INSTALL SUBLIMETEXT INTO DOCKER AND UBUNTU
